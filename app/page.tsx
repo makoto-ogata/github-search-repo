2f077e48a7998a5c8838af0e37df235b5711a150
Makoto Ogata <ogatamakoto1976@gmail.com>
2026-05-05 19:10:33 +0900

    Create header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { SearchForm } from "@/app/components/SearchForm";
 import { searchRepositories } from "@/lib/github";
+import Link from "next/link";
+import Image from "next/image";
 
 interface Props {
 	searchParams: Promise<{ q?: string }>;
@@ -10,35 +12,42 @@ export default async function Home({ searchParams }: Props) {
 	const repos = q ? await searchRepositories(q) : [];
 
 	return (
-		<div className='flex flex-col flex-1 items-center justify-start bg-zinc-50 font-sans dark:bg-black'>
-			<main className='flex flex-1 w-full max-w-3xl flex-col items-center justify-start py-12 px-4 sm:px-16 bg-white dark:bg-black sm:items-start'>
-				<SearchForm defaultValue={q ?? ""} />
-				{q && repos.length > 0 && (
-					<ul className='mt-8 w-full divide-y divide-gray-200'>
-						{repos.map((repo) => (
-							<li key={repo.id} className='py-4'>
-								<a
-									href={`/repos/${repo.owner.login}/${repo.name}`}
-									className='text-blue-600 hover:underline font-medium'>
+		<main className='my-12 mx-auto w-full max-w-3xl px-4'>
+			<SearchForm defaultValue={q ?? ""} />
+			{q && repos.length > 0 && (
+				<ul className='mt-8 w-full grid gap-4'>
+					{repos.map((repo) => (
+						<li key={repo.id}>
+							<Link
+								href={`/repos/${repo.owner.login}/${repo.name}`}
+								className='font-medium py-4 break-all flex gap-3 w-full items-center justify-start rounded-3xl border border-gray-300 px-4 hover:bg-gray-50 dark:border-zinc-700 dark:hover:bg-zinc-800'>
+								<Image
+									src={repo.owner.avatar_url}
+									alt={repo.owner.login}
+									width={40}
+									height={40}
+									className='shrink-0'
+								/>
+								<div className='grow'>
 									{repo.full_name}
-								</a>
-								<p className='text-sm text-gray-500 mt-1'>{repo.description}</p>
-								<div className='text-sm text-gray-400 mt-1'>
-									{repo.language && (
-										<span className='mr-4'>{repo.language}</span>
-									)}
-									<span>⭐ {repo.stargazers_count}</span>
+									<p className='text-sm text-gray-500 mt-1'>
+										{repo.description}
+									</p>
+									<div className='text-sm text-gray-400 mt-1'>
+										{repo.language && (
+											<span className='mr-4'>{repo.language}</span>
+										)}
+										<span>⭐ {repo.stargazers_count}</span>
+									</div>
 								</div>
-							</li>
-						))}
-					</ul>
-				)}
-				{q && repos.length === 0 && (
-					<p className='mt-8 text-center text-gray-500'>
-						No repositories found.
-					</p>
-				)}
-			</main>
-		</div>
+							</Link>
+						</li>
+					))}
+				</ul>
+			)}
+			{q && repos.length === 0 && (
+				<p className='mt-8 text-center text-gray-500'>No repositories found.</p>
+			)}
+		</main>
 	);
 }
